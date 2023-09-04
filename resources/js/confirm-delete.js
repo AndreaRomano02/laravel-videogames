4d7a804adc53ea2ffbe27d1a8ab2c55a09c6a3b0
stefansimereaa <stefanosimerea16@gmail.com>
2023-09-04 17:19:08 +0200

    fixed code

--- resources/js/confirm-delete.js
+++ resources/js/confirm-delete.js
@@ -5,11 +5,11 @@ const deleteButton = document.getElementById('deleteButton');
 deleteForm.forEach(form => {
     form.addEventListener('submit', e => {
         e.preventDefault();
-        const name = form.dataset.name;
+        const name = form.dataset.title;
 
 
 
-        modalBody.innerText = `Sei sicuro di voler eliminare ${name}`
+        modalBody.innerText = `Sei sicuro di voler eliminare "${name}"`
 
         deleteButton.addEventListener("click", function () {
             form.submit();
