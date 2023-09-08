f31c8b02c47b4b4b664d395d834dad167baaa9d6
cloudhuh <claudiu.huhulia@gmail.com>
2023-09-08 11:54:16 +0200

    create publisher entity

---
+++ database/seeders/PublisherSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Publisher;
+use Faker\Generator;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PublisherSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(Generator $faker): void
+    {
+        $labels = ['Ubisoft', 'Sega', 'Nintendo', 'Activision', 'Electronic Arts'];
+        foreach ($labels as $label) {
+            $publisher = new Publisher();
+            $publisher->label = $label;
+            $publisher->color = $faker->hexColor();
+
+            $publisher->save();
+        }
+    }
+}
