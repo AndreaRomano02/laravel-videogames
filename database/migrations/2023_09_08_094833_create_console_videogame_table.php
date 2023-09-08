0886471ab093e815a85eac4e4ec8e5d6bae4261c
Deltafortal <aldolucchetta21@gmail.com>
2023-09-08 11:51:46 +0200

    Added console entity

--- database/migrations/2023_09_08_094833_create_console_videogame_table.php
+++ database/migrations/2023_09_08_094833_create_console_videogame_table.php
@@ -11,9 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('consoles', function (Blueprint $table) {
+        Schema::create('console_videogame', function (Blueprint $table) {
             $table->id();
-            $table->string('label', 30);
+            $table->foreignId('console_id')->constrained()->onDelete('cascade');
+            $table->foreignId('videogame_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
@@ -23,6 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('consoles');
+        Schema::dropIfExists('console_videogame');
     }
 };
